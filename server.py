79cc66a7b81305631abac3de491ceb988f48c928
Jack <jft360@gmail.com>
2021-10-18 19:06:46 -0500

    use dnslib

--- server.py
+++ server.py
@@ -1,49 +1,138 @@
-from socketserver import BaseRequestHandler
+#!/bin/python3
+# # -*- coding: utf-8 -*-
 
-SERVER_PORT = 53
-SERVER_IP = '0.0.0.0'
+from __future__ import print_function
+
+import socket
+import struct
+
+from dnslib import DNSRecord, RCODE
+from dnslib.server import DNSServer, DNSHandler, BaseResolver, DNSLogger
 
 
-class DNSHandler(BaseRequestHandler):
+class ProxyResolver(BaseResolver):
     """
-    DNS Protocol:
-        Header section:
-            16 bits for header
-                1 bit for message type (0 - query, 1 - response)
-                4 bits for opcode (???)
-                1 bit for isAuthoritative (0 - no, 1 - yes; should almost always be 0 for this server)
-                1 bit for truncation (0 - no split, 1 - msg was split from size)
-                1 bit for recursion desired (0 - query should not be recursive, 1 - query is recursive)
-                1 bit for recursion availability (0 - server cannot perform recursion query, 1 - server can)
-                1 bit for reserved (0 - ?; should always be 0)
-                [Seems that this isn't exact, the next two bits could be 0 or they can be set correctly; some resources
-                have these marked as reserved still.]
-                1 bit for isAnswerAuthenticated (0 - not, 1 - is; should probably be 0 for this server)
-                1 bit for is non-authenticated data accepted (0 - non-authenticated data not accepted, 1 - it is accepted)
-                4 bit for reply code (????)
-            16 bits for number of questions
-            16 bits for number of answers
-            16 bits for number of authoritative records
-            16 bits for number of additional records
-        Query section:
-            ?? bits for name (URL is split into labels, for each label, the characters are encoded with ascii value,
-                              then the label is succeeded by its length in bytes. Repeat for each label. To finish this
-                              section, it is terminated with 0)
-                              (https://routley.io/posts/hand-writing-dns-messages/) here for more info.
-            16 bits for record type (only expecting A which means value of 0x01)
-            16 bits for query class (always expecting IN, so value of 0x01)
-        Answer section:
-            16 bits for name reference (first two bits are always 1, rest of the 14 bits are unsigned integer,
-                                        said integer refers to byte offset from the beginning of the message
-                                        to the 1st occurrence of the name.)
-            16 bits for record type
-            16 bits for query class
-            32 bits for time to live
-            16 bits for data length(value in bytes)
-            ?? bits for address (length is equal to data length)
-    """
-    def handle(self) -> None:
-        received, socket = self.request
-        print(f'{self.client_address[0]} sent {received.decode("utf-8")}.')
+        Proxy resolver - passes all requests to upstream DNS server and
+        returns response
 
-        socket.sendto(b'hi', self.client_address)
+        Note that the request/response will be each be decoded/re-encoded
+        twice:
+
+        a) Request packet received by DNSHandler and parsed into DNSRecord
+        b) DNSRecord passed to ProxyResolver, serialised back into packet
+           and sent to upstream DNS server
+        c) Upstream DNS server returns response packet which is parsed into
+           DNSRecord
+        d) ProxyResolver returns DNSRecord to DNSHandler which re-serialises
+           this into packet and returns to client
+
+        In practice this is actually fairly useful for testing but for a
+        'real' transparent proxy option the DNSHandler logic needs to be
+        modified (see PassthroughDNSHandler)
+
+    """
+
+    def __init__(self, address, port, timeout=0):
+        self.address = address
+        self.port = port
+        self.timeout = timeout
+
+    def resolve(self, request, handler):
+        try:
+            if handler.protocol == 'udp':
+                proxy_r = request.send(self.address, self.port,
+                                       timeout=self.timeout)
+            else:
+                proxy_r = request.send(self.address, self.port,
+                                       tcp=True, timeout=self.timeout)
+            reply = DNSRecord.parse(proxy_r)
+        except socket.timeout:
+            reply = request.reply()
+            reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
+
+        return reply
+
+
+def send_tcp(data, host, port):
+    """
+        Helper function to send/receive DNS TCP request
+        (in/out packets will have prepended TCP length header)
+    """
+    sock = None
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.sendall(data)
+        response = sock.recv(8192)
+        length = struct.unpack("!H", bytes(response[:2]))[0]
+        while len(response) - 2 < length:
+            response += sock.recv(8192)
+        return response
+    finally:
+        if (sock is not None):
+            sock.close()
+
+
+def send_udp(data, host, port):
+    """
+        Helper function to send/receive DNS UDP request
+    """
+    sock = None
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(data, (host, port))
+        response, server = sock.recvfrom(8192)
+        return response
+    finally:
+        if (sock is not None):
+            sock.close()
+
+
+if __name__ == '__main__':
+
+    import argparse
+    import time
+
+    p = argparse.ArgumentParser(description="DNS Proxy")
+    p.add_argument("--port", "-p", type=int, default=53,
+                   metavar="<port>",
+                   help="Local proxy port (default:53)")
+    p.add_argument("--address", "-a", default="",
+                   metavar="<address>",
+                   help="Local proxy listen address (default:all)")
+    p.add_argument("--upstream", "-u", default="8.8.8.8:53",
+                   metavar="<dns server:port>",
+                   help="Upstream DNS server:port (default:8.8.8.8:53)")
+    p.add_argument("--tcp", action='store_true', default=False,
+                   help="TCP proxy (default: UDP only)")
+    p.add_argument("--timeout", "-o", type=float, default=5,
+                   metavar="<timeout>",
+                   help="Upstream timeout (default: 5s)")
+    p.add_argument("--passthrough", action='store_true', default=False,
+                   help="Dont decode/re-encode request/response (default: off)")
+    p.add_argument("--log", default="request,reply,truncated,error",
+                   help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)")
+    p.add_argument("--log-prefix", action='store_true', default=False,
+                   help="Log prefix (timestamp/handler/resolver) (default: False)")
+    args = p.parse_args()
+
+    args.dns, _, args.dns_port = args.upstream.partition(':')
+    args.dns_port = int(args.dns_port or 53)
+
+    print("Starting Proxy Resolver (%s:%d -> %s:%d) [%s]" % (
+        args.address or "*", args.port,
+        args.dns, args.dns_port,
+        "UDP/TCP" if args.tcp else "UDP"))
+
+    resolver = ProxyResolver(args.dns, args.dns_port, args.timeout)
+    handler = DNSHandler
+    logger = DNSLogger(args.log, args.log_prefix)
+    udp_server = DNSServer(resolver,
+                           port=args.port,
+                           address=args.address,
+                           logger=logger,
+                           handler=handler)
+    udp_server.start_thread()
+
+    while udp_server.isAlive():
+        time.sleep(1)
